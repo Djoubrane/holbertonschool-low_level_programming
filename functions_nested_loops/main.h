5b9f21630d5f52aa2ee1a24427af97398a04814f
Djoubrane <9554@holbertonstudents.com>
2024-06-21 08:32:29 +0000

    task1

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -6,6 +5,3 @@ int _putchar(char c);
 void print_alphabet(void);
 
 #endif /* MAIN_H */
-=======
-int _putchar(char c); void print_alphabet(void);
->>>>>>> c6d4e8eec96b8ca2eb78c176cacb024d1ede20ac
