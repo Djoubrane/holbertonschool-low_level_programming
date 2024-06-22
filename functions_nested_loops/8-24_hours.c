7c323c5579afbc0d85f6083625ca2b98b54c7088
« Djoubrane » <9554@holbertonstudents.com>
2024-06-22 18:59:56 +0000

    test

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,20 +1,24 @@
 #include "main.h"
 
 /**
- * jack_bauer - Affiche chaque minute de la journée de 00:00 à 23:59.
+ * jack_bauer - Prints every minute of the day of
+ *              Jack Bauer, starting from 00:00 to 23:59.
  */
 
 void jack_bauer(void)
-
-	for (hour = 0; hour < 24; hour++)
 {
-	for (minute = 0; minute < 60; minute++)
+	int hour, minute;
+
+	for (hour = 0; hour <= 23; hour++)
 	{
-	_putchar((hour / 10) + '0');
-	_putchar((hour % 10) + '0');
-	_putchar(':');
-	_putchar((minute / 10) + '0');
-	_putchar((minute % 10) + '0');
-	_putchar('\n');
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
 	}
 }
