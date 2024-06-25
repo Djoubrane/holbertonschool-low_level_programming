a0462889bea3c17f964c280ce23a27a572780ae7
« Djoubrane » <9554@holbertonstudents.com>
2024-06-25 08:36:11 +0000

    test

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10x les nombres de 0 à 14 suivis d'une nouvelle ligne
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+	char buffer[3];
+
+for (i = 0; i < 10; i++)
+	{
+
+	for (j = 0; j <= 14; j++)
+	{
+	if (j >= 10)
+		{
+		buffer[0] = '1';
+		buffer[1] = '0' + (j % 10);
+		buffer[2] = '\0';
+		_putchar(buffer[0]);
+		_putchar(buffer[1]);
+		}
+		else
+		{
+		buffer[0] = '0' + j;
+		buffer[1] = '\0';
+		_putchar(buffer[0]);
+		}
+	}
+	_putchar('\n');
+	}
+}
