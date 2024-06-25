8b516fe405c4305c672269a1e3e769da476f232b
« Djoubrane » <9554@holbertonstudents.com>
2024-06-25 12:06:36 +0000

    test

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - Imprime un carré suivi d'une nouvelle ligne
+ * @size: Taille du carré
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (i = 0; i < size; i++)
+	{
+	for (j = 0; j < size; j++)
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
+	}
+	}
+}
